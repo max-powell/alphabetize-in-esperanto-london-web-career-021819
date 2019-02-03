9d42b957abfc76709050375c7784035eee2abb65
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-03 22:54:44 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,10 @@
+require "pry"
+
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   arr.sort_by do |word|
+    binding.pry
     ESPERANTO_ALPHABET.index(word)
   end
 end
